ee65d59342e4f0596fd6ec5ec8cdc5ac77795f71
brianvikonura <vikonura@gmail.com>
2024-05-27 18:50:21 +0700

    feat: Added role-based permissions for secure page access

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,4 +1,3 @@
-<!-- partial:partials/_navbar.html -->
 <nav class="navbar default-layout-navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
     <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
         <a class="navbar-brand brand-logo" href={{ route('home') }}><img src="{{ asset('images/logo.svg') }}"
@@ -22,17 +21,29 @@
                         <p class="mb-1 text-black">Hi, {{ Auth::user()->name }}</p>
                     </div>
                 </a>
-                <div class="dropdown-menu navbar-dropdown" aria-labelledby="profileDropdown">
-                    <a class="dropdown-item" href="{{ route('settings.index') }}">
-                        <i class="mdi mdi-cached me-2 text-success"></i> Settings </a>
-                    <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="#"
-                        onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                        <i class="mdi mdi-logout me-2 text-primary"></i> Signout </a>
-                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                        @csrf
-                    </form>
-                </div>
+                @if (Auth::user()->role == 'superadmin')
+                    <div class="dropdown-menu navbar-dropdown" aria-labelledby="profileDropdown">
+                        <a class="dropdown-item" href="{{ route('settings.index') }}">
+                            <i class="mdi mdi-cached me-2 text-success"></i> Settings </a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#"
+                            onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                            <i class="mdi mdi-logout me-2 text-primary"></i> Signout </a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                            @csrf
+                        </form>
+                    </div>
+                @elseif (Auth::user()->role == 'admin')
+                    <div class="dropdown-menu navbar-dropdown" aria-labelledby="profileDropdown">
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#"
+                            onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                            <i class="mdi mdi-logout me-2 text-primary"></i> Signout </a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                            @csrf
+                        </form>
+                    </div>
+                @endif
             </li>
             <li class="nav-item d-none d-lg-block full-screen-link">
                 <a class="nav-link">
--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -2,89 +2,163 @@
     <!-- partial:partials/_sidebar.html -->
     <nav class="sidebar sidebar-offcanvas" id="sidebar">
         <ul class="nav">
-            <li class="nav-item nav-profile">
-                <a href="{{ route('home') }}" class="nav-link">
-                    <div class="nav-profile-image">
-                        <img src="{{ asset('images/faces/face1.jpg') }}" alt="profile">
-                        <span class="login-status online"></span>
-                        <!--change to offline or busy as needed-->
+            @if (Auth::user()->role == 'superadmin')
+                <li class="nav-item nav-profile">
+                    <a href="{{ route('home') }}" class="nav-link">
+                        <div class="nav-profile-image">
+                            <img src="{{ asset('images/faces/face1.jpg') }}" alt="profile">
+                            <span class="login-status online"></span>
+                            <!--change to offline or busy as needed-->
+                        </div>
+                        <div class="nav-profile-text d-flex flex-column">
+                            <span class="font-weight-bold mb-2">{{ Auth::user()->name }}</span>
+                            <span class="text-secondary text-small">{{ Auth::user()->role }}</span>
+                        </div>
+                        <i class="mdi mdi-bookmark-check text-success nav-profile-badge"></i>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('home') }}">
+                        <span class="menu-title">Dashboard</span>
+                        <i class="mdi mdi-home menu-icon"></i>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" data-bs-toggle="collapse" href="#master-barang" aria-expanded="false"
+                        aria-controls="master-barang">
+                        <span class="menu-title">Master Barang</span>
+                        <i class="menu-arrow"></i>
+                        <i class="mdi mdi-package menu-icon"></i>
+                    </a>
+                    <div class="collapse" id="master-barang">
+                        <ul class="nav flex-column sub-menu">
+                            <li class="nav-item"> <a class="nav-link" href="{{ route('jenisBarang.index') }}">Jenis</a>
+                            </li>
+                            <li class="nav-item"> <a class="nav-link" href="{{ route('satuan.index') }}">Satuan</a></li>
+                            <li class="nav-item"> <a class="nav-link" href="{{ route('barang.index') }}">Barang</a></li>
+                        </ul>
                     </div>
-                    <div class="nav-profile-text d-flex flex-column">
-                        <span class="font-weight-bold mb-2">{{ Auth::user()->name }}</span>
-                        <span class="text-secondary text-small">{{ Auth::user()->role }}</span>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('customer.index') }}">
+                        <span class="menu-title">Customer</span>
+                        <i class="mdi mdi-account-group-outline menu-icon"></i>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('ekspedisi.index') }}">
+                        <span class="menu-title">Ekspedisi</span>
+                        <i class="mdi mdi-account-group-outline menu-icon"></i>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" data-bs-toggle="collapse" href="#transaksi" aria-expanded="false"
+                        aria-controls="transaksi">
+                        <span class="menu-title">Transaksi</span>
+                        <i class="menu-arrow"></i>
+                        <i class="mdi mdi-cached menu-icon"></i>
+                    </a>
+                    <div class="collapse" id="transaksi">
+                        <ul class="nav flex-column sub-menu">
+                            <li class="nav-item"> <a class="nav-link" href="{{ route('barangmasuk.index') }}">Barang
+                                    Masuk</a></li>
+                            <li class="nav-item"> <a class="nav-link" href="{{ route('barangkeluar.index') }}">Barang
+                                    Keluar</a></li>
+                        </ul>
                     </div>
-                    <i class="mdi mdi-bookmark-check text-success nav-profile-badge"></i>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="{{ route('home') }}">
-                    <span class="menu-title">Dashboard</span>
-                    <i class="mdi mdi-home menu-icon"></i>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" data-bs-toggle="collapse" href="#master-barang" aria-expanded="false"
-                    aria-controls="master-barang">
-                    <span class="menu-title">Master Barang</span>
-                    <i class="menu-arrow"></i>
-                    <i class="mdi mdi-package menu-icon"></i>
-                </a>
-                <div class="collapse" id="master-barang">
-                    <ul class="nav flex-column sub-menu">
-                        <li class="nav-item"> <a class="nav-link" href="{{ route('jenisBarang.index') }}">Jenis</a></li>
-                        <li class="nav-item"> <a class="nav-link" href="{{ route('satuan.index') }}">Satuan</a></li>
-                        <li class="nav-item"> <a class="nav-link" href="{{ route('barang.index') }}">Barang</a></li>
-                    </ul>
-                </div>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="{{ route('customer.index') }}">
-                    <span class="menu-title">Customer</span>
-                    <i class="mdi mdi-account-group-outline menu-icon"></i>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="{{ route('ekspedisi.index') }}">
-                    <span class="menu-title">Ekspedisi</span>
-                    <i class="mdi mdi-account-group-outline menu-icon"></i>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" data-bs-toggle="collapse" href="#transaksi" aria-expanded="false"
-                    aria-controls="transaksi">
-                    <span class="menu-title">Transaksi</span>
-                    <i class="menu-arrow"></i>
-                    <i class="mdi mdi-cached menu-icon"></i>
-                </a>
-                <div class="collapse" id="transaksi">
-                    <ul class="nav flex-column sub-menu">
-                        <li class="nav-item"> <a class="nav-link" href="{{ route('barangmasuk.index') }}">Barang
-                                Masuk</a></li>
-                        <li class="nav-item"> <a class="nav-link" href="{{ route('barangkeluar.index') }}">Barang
-                                Keluar</a></li>
-                    </ul>
-                </div>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" data-bs-toggle="collapse" href="#laporan" aria-expanded="false"
-                    aria-controls="laporan">
-                    <span class="menu-title">Laporan</span>
-                    <i class="menu-arrow"></i>
-                    <i class="mdi mdi-printer menu-icon"></i>
-                </a>
-                <div class="collapse" id="laporan">
-                    <ul class="nav flex-column sub-menu">
-                        <li class="nav-item"> <a class="nav-link" href="{{ route('laporan.barangmasuk.index') }}">Laporan Barang Masuk</a></li>
-                        <li class="nav-item"> <a class="nav-link" href="{{ route('laporan.barangkeluar.index') }}">Laporan Barang Keluar</a></li>
-                        <li class="nav-item"> <a class="nav-link" href="{{ route('laporan.stokbarang.index') }}">Laporan Stok Barang</a></li>
-                    </ul>
-                </div>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="{{ route('pengguna.index') }}">
-                    <span class="menu-title">Pengguna</span>
-                    <i class="mdi mdi-account-search menu-icon"></i>
-                </a>
-            </li>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" data-bs-toggle="collapse" href="#laporan" aria-expanded="false"
+                        aria-controls="laporan">
+                        <span class="menu-title">Laporan</span>
+                        <i class="menu-arrow"></i>
+                        <i class="mdi mdi-printer menu-icon"></i>
+                    </a>
+                    <div class="collapse" id="laporan">
+                        <ul class="nav flex-column sub-menu">
+                            <li class="nav-item"> <a class="nav-link"
+                                    href="{{ route('laporan.barangmasuk.index') }}">Laporan Barang Masuk</a></li>
+                            <li class="nav-item"> <a class="nav-link"
+                                    href="{{ route('laporan.barangkeluar.index') }}">Laporan Barang Keluar</a></li>
+                            <li class="nav-item"> <a class="nav-link"
+                                    href="{{ route('laporan.stokbarang.index') }}">Laporan Stok Barang</a></li>
+                        </ul>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('pengguna.index') }}">
+                        <span class="menu-title">Pengguna</span>
+                        <i class="mdi mdi-account-search menu-icon"></i>
+                    </a>
+                </li>
+            @elseif (Auth::user()->role == 'admin')
+                <li class="nav-item nav-profile">
+                    <a href="{{ route('home') }}" class="nav-link">
+                        <div class="nav-profile-image">
+                            <img src="{{ asset('images/faces/face1.jpg') }}" alt="profile">
+                            <span class="login-status online"></span>
+                            <!--change to offline or busy as needed-->
+                        </div>
+                        <div class="nav-profile-text d-flex flex-column">
+                            <span class="font-weight-bold mb-2">{{ Auth::user()->name }}</span>
+                            <span class="text-secondary text-small">{{ Auth::user()->role }}</span>
+                        </div>
+                        <i class="mdi mdi-bookmark-check text-success nav-profile-badge"></i>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('home') }}">
+                        <span class="menu-title">Dashboard</span>
+                        <i class="mdi mdi-home menu-icon"></i>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" data-bs-toggle="collapse" href="#master-barang" aria-expanded="false"
+                        aria-controls="master-barang">
+                        <span class="menu-title">Master Barang</span>
+                        <i class="menu-arrow"></i>
+                        <i class="mdi mdi-package menu-icon"></i>
+                    </a>
+                    <div class="collapse" id="master-barang">
+                        <ul class="nav flex-column sub-menu">
+                            <li class="nav-item"> <a class="nav-link"
+                                    href="{{ route('jenisBarang.index') }}">Jenis</a></li>
+                            <li class="nav-item"> <a class="nav-link" href="{{ route('satuan.index') }}">Satuan</a>
+                            </li>
+                            <li class="nav-item"> <a class="nav-link" href="{{ route('barang.index') }}">Barang</a>
+                            </li>
+                        </ul>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('customer.index') }}">
+                        <span class="menu-title">Customer</span>
+                        <i class="mdi mdi-account-group-outline menu-icon"></i>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('ekspedisi.index') }}">
+                        <span class="menu-title">Ekspedisi</span>
+                        <i class="mdi mdi-account-group-outline menu-icon"></i>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" data-bs-toggle="collapse" href="#transaksi" aria-expanded="false"
+                        aria-controls="transaksi">
+                        <span class="menu-title">Transaksi</span>
+                        <i class="menu-arrow"></i>
+                        <i class="mdi mdi-cached menu-icon"></i>
+                    </a>
+                    <div class="collapse" id="transaksi">
+                        <ul class="nav flex-column sub-menu">
+                            <li class="nav-item"> <a class="nav-link" href="{{ route('barangmasuk.index') }}">Barang
+                                    Masuk</a></li>
+                            <li class="nav-item"> <a class="nav-link"
+                                    href="{{ route('barangkeluar.index') }}">Barang
+                                    Keluar</a></li>
+                        </ul>
+                    </div>
+                </li>
+            @endif
         </ul>
     </nav>
